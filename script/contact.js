b5680d0f27bba597cfcfe47a43a767a13a814624
Ambroise Donnet <ambroisemathieu.donnet@gmail.com>
2024-12-15 16:02:46 +0100

    Changement complet du site

--- script/contact.js
+++ script/contact.js
@@ -16,3 +16,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     observer.observe(lastSection);
 });
+
+// Gestion du clic sur le bouton Email
+const emailButton = document.getElementById("email-btn");
+const emailPopup = document.getElementById("email-popup");
+
+emailButton.addEventListener("click", () => {
+    // Copier l'adresse email dans le presse-papier
+    navigator.clipboard.writeText("ambroise.donnet.pro@outlook.fr").then(() => {
+        // Afficher la popup
+        emailPopup.style.display = "block";
+        // Masquer la popup après 2 secondes
+        setTimeout(() => {
+            emailPopup.style.display = "none";
+        }, 2000);
+    });
+});
+
+// Activer les tooltips Bootstrap sur la page
+document.addEventListener('DOMContentLoaded', function () {
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
